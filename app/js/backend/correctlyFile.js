b03aa914008df2e0c7f781cfdae76165b67c8d4a
ThunderInTheGrass <propeller8924@gmail.com>
2024-06-01 22:20:12 +0800

    скрипт в процессе, пока массив - это урок, в нем объекты по количеству фраз, осталось внедрить в объекты id и сами фразы

---
+++ app/js/backend/correctlyFile.js
@@ -0,0 +1,87 @@
+[
+  { id: 1, phraseRu: "Я знаю", phraseEn: "I know" },
+  { id: 2, phraseRu: "Я не знаю", phraseEn: "I don't know" },
+  { id: 3, phraseRu: "Мы понимаем", phraseEn: "We understand" },
+  { id: 4, phraseRu: "Мы не понимаем", phraseEn: "We don't understand" },
+  {
+    id: 5,
+    phraseRu: "Ты пытаешься выучить английский",
+    phraseEn: "You try to learn English",
+  },
+  {
+    id: 6,
+    phraseRu: "Ты не пытаешься выучить английский",
+    phraseEn: "You don't try to learn English",
+  },
+  { id: 7, phraseRu: "Они помогают мне", phraseEn: "They help me" },
+  { id: 8, phraseRu: "Они не помогают мне", phraseEn: "They don't help me" },
+  { id: 9, phraseRu: "Я согласен с тобой", phraseEn: "I agree with you" },
+  {
+    id: 10,
+    phraseRu: "Я не согласен с тобой",
+    phraseEn: "I don't agree with you",
+  },
+  { id: 11, phraseRu: "Мне нравится это", phraseEn: "I like it" },
+  { id: 12, phraseRu: "Мне не нравится это", phraseEn: "I don't like it" },
+  { id: 13, phraseRu: "Я хочу учиться там", phraseEn: "I want to study there" },
+  {
+    id: 14,
+    phraseRu: "Я не хочу учиться там",
+    phraseEn: "I don't want study there",
+  },
+  { id: 15, phraseRu: "Я хочу обсудить это", phraseEn: "I want to discuss it" },
+  {
+    id: 16,
+    phraseRu: "Я не хочу обсуждать это",
+    phraseEn: "I don't want to discuss it",
+  },
+  {
+    id: 17,
+    phraseRu: "Я хочу сделать это прямо сейчас",
+    phraseEn: "I want to do it right now",
+  },
+  {
+    id: 18,
+    phraseRu: "Я не хочу делать это прямо сейчас",
+    phraseEn: "I don't to do it right now",
+  },
+  {
+    id: 19,
+    phraseRu: "Я хочу сделать это позже",
+    phraseEn: "I want to do it later",
+  },
+  {
+    id: 20,
+    phraseRu: "Я не хочу говорить это",
+    phraseEn: "I don't want to say it",
+  },
+  {
+    id: 21,
+    phraseRu: "Я не хочу говорить тебе эту вещь",
+    phraseEn: "I won't want to tell you this thing",
+  },
+  {
+    id: 22,
+    phraseRu: "Я не хочу говорить тебе об этой проблеме",
+    phraseEn: "I don't want to tell you about rhis problem",
+  },
+  {
+    id: 23,
+    phraseRu: "Я не хочу думать об этом",
+    phraseEn: "I don't want to think about it",
+  },
+  { id: 24, phraseRu: "Он знает это", phraseEn: "He knows it" },
+  { id: 25, phraseRu: "Он не знает это", phraseEn: "He doesn't know it" },
+  {
+    id: 26,
+    phraseRu: "Она не хочет обсуждать это",
+    phraseEn: "She doesn't want to discuss it",
+  },
+  { id: 27, phraseRu: "Это не помогает мне", phraseEn: "It doesn't help me" },
+  {
+    id: 28,
+    phraseRu: "Это не кажется странным",
+    phraseEn: "It doesn't seem strange",
+  },
+  { id: 29, phraseRu: "Это не работает", phraseEn: "It doesn't work" },
+];
